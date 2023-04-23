dd6f21b802956b37c80dc995cafe19d66a70ae8c
josermf2 <josermf2@al.insper.edu.br>
2023-04-23 17:40:25 -0300

    primeira versao da entrega 2.2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-reserved_words = ['println']
+reserved_words = ['println', 'while', 'if', 'end', 'readline']
 
 class Token:
     def __init__(self, _type, value):
@@ -36,6 +36,8 @@ class UnOp(Node):
     def Evaluate(self):
         if self.value == '-':
             return - self.children[0].Evaluate()
+        if self.value == '!':
+            return not self.children[0].Evaluate()
         return self.children[0].Evaluate()
 
 class BinOp(Node):
@@ -52,6 +54,16 @@ class BinOp(Node):
             return self.children[0].Evaluate() * self.children[1].Evaluate()
         elif self.value == '/':
             return self.children[0].Evaluate() // self.children[1].Evaluate()
+        elif self.value == '==':
+            return self.children[0].Evaluate() == self.children[1].Evaluate()
+        elif self.value == '<':
+            return self.children[0].Evaluate() < self.children[1].Evaluate()
+        elif self.value == '>':
+            return self.children[0].Evaluate() > self.children[1].Evaluate()
+        elif self.value == '&&':
+            return self.children[0].Evaluate() and self.children[1].Evaluate()
+        elif self.value == '||':
+            return self.children[0].Evaluate() or self.children[1].Evaluate()
         
 
 class IntVal(Node):
@@ -101,6 +113,34 @@ class Block(Node):
     def Evaluate(self):
         for child in self.children:
             child.Evaluate()
+
+class While(Node):
+    def __init__(self, value, children):
+        self.value = value
+        self.children = children
+    
+    def Evaluate(self):
+        while self.children[0].Evaluate():
+            self.children[1].Evaluate()
+
+class If(Node):
+    def __init__(self, value, children):
+        self.value = value
+        self.children = children
+    
+    def Evaluate(self):
+        if self.children[0].Evaluate():
+            self.children[1].Evaluate()
+        elif len(self.children) == 3:
+            self.children[2].Evaluate()
+
+class Readline(Node):
+    def __init__(self):
+        pass
+
+    def evaluate(self):
+        return int(input())
+
 
 class SymbolTable:
     def __init__(self):
@@ -161,8 +201,35 @@ class Tokenizer:
             self.next = Token('NEW_LINE', '\n')
             self.position += 1
         elif self.position < original_size and self.source[self.position] == '=':
+            if self.position + 1 < original_size and self.source[self.position+1] == '=':
+                self.next = Token('EQUAL', '==')
+                self.position += 2
+            else:
+                self.next = Token('ASSIGN', '=')
+                self.position += 1
             self.next = Token('ASSIGN', '=')
             self.position += 1
+        elif self.position < original_size and self.source[self.position] == '>':
+            self.next = Token('GREATER', '>')
+            self.position += 1
+        elif self.position < original_size and self.source[self.position] == '<':
+            self.next = Token('LESS', '<')
+            self.position += 1
+        elif self.position < original_size and self.source[self.position] == '!':
+            self.next = Token('DIFFERENT', '!')
+            self.position += 1
+        elif self.position < original_size and self.source[self.position] == '&':
+            if self.position + 1 < original_size and self.source[self.position+1] == '&':
+                self.next = Token('AND', '&&')
+                self.position += 2
+            else:
+                raise Exception('Algo de estranho aconteceu, confira a entrada')
+        elif self.position < original_size and self.source[self.position] == '|':
+            if self.position + 1 < original_size and self.source[self.position+1] == '|':
+                self.next = Token('OR', '||')
+                self.position += 2
+            else:
+                raise Exception('Algo de estranho aconteceu, confira a entrada')
         else:
             raise Exception('Algo de estranho aconteceu, confira a entrada')
         return self.next   
@@ -199,6 +266,43 @@ class Parser:
                     return Print('PRINT', [to_print]) 
                 else:
                     raise Exception('Algo de estranho aconteceu, confira a entrada')
+        elif self.tokenizer.next.type == 'RESERVED' and self.tokenizer.next.value == 'while':
+            self.tokenizer.selectNext()
+            condition = self.parserRelExpr()   
+            if self.tokenizer.next.type == 'NEW_LINE':
+                self.tokenizer.selectNext()
+                body = Block('BLOCK', [])
+                while self.tokenizer.next.value != 'end':
+                    body.children.append(self.parseStatement())
+                if self.tokenizer.next.type == 'RESERVED' and self.tokenizer.next.value == 'end':
+                    self.tokenizer.selectNext()
+                    return While('WHILE', [condition, body])
+        elif self.tokenizer.next.type == 'RESERVED' and self.tokenizer.next.value == 'if':
+            self.tokenizer.selectNext()
+            condition = self.parserRelExpr()
+
+            if self.tokenizer.next.type == 'NEW_LINE':
+                self.tokenizer.selectNext()
+                body = Block('BLOCK', [])
+                while self.tokenizer.next.value != 'end' and self.tokenizer.next.value != 'else':
+                    body.children.append(self.parseStatement())
+                if self.tokenizer.next.type == 'RESERVED' and self.tokenizer.next.value == 'else':
+                    self.tokenizer.selectNext()
+                    if self.tokenizer.next.type == 'NEW_LINE':
+                        self.tokenizer.selectNext()
+                        else_body = Block('BLOCK', [])
+                        while self.tokenizer.next.value != 'end':
+                            else_body.children.append(self.parseStatement())
+                        if self.tokenizer.next.type == 'RESERVED' and self.tokenizer.next.value == 'end':
+                            self.tokenizer.selectNext()
+                            return If('IF', [condition, body, else_body])
+                        else:
+                            raise Exception('Algo de estranho aconteceu, confira a entrada')
+                elif self.tokenizer.next.type == 'RESERVED' and self.tokenizer.next.value == 'end':
+                    self.tokenizer.selectNext()
+                    return If('IF', [condition, body])
+                else:
+                    raise Exception('Algo de estranho aconteceu, confira a entrada')
         else:
             raise Exception('Algo de estranho aconteceu, confira a entrada')
 
@@ -215,12 +319,27 @@ class Parser:
             self.tokenizer.selectNext()
             result = self.parseFactor()
             return UnOp('-', [result])
+        elif self.tokenizer.next.type == 'DIFFERENT':
+            self.tokenizer.selectNext()
+            result = self.parseFactor()
+            return UnOp('!', [result])
         elif self.tokenizer.next.type == 'O_PAR':
             self.tokenizer.selectNext()
-            result = self.parseExpression()
+            result = self.parserRelExpr()
             if self.tokenizer.next.type == 'C_PAR':
                 self.tokenizer.selectNext()
                 return result
+            else:
+                raise Exception('Algo de estranho aconteceu, confira a entrada')
+        elif self.tokenizer.next.type == 'RESERVED' and self.tokenizer.next.value == 'readline':
+            self.tokenizer.selectNext()
+            if self.tokenizer.next.type == 'O_PAR':
+                self.tokenizer.selectNext()
+                if self.tokenizer.next.type == 'C_PAR':
+                    self.tokenizer.selectNext()
+                    return Readline()
+                else:
+                    raise Exception('Algo de estranho aconteceu, confira a entrada')
             else:
                 raise Exception('Algo de estranho aconteceu, confira a entrada')
         elif self.tokenizer.next.type == 'IDENTIFIER':
@@ -232,7 +351,7 @@ class Parser:
 
     def parseTerm(self):
         result = self.parseFactor()
-        while self.tokenizer.next.type == 'MUL' or self.tokenizer.next.type == 'DIV':
+        while self.tokenizer.next.type == 'MUL' or self.tokenizer.next.type == 'DIV' or self.tokenizer.next.type == 'AND':
             if self.tokenizer.next.type == 'MUL':
                 self.tokenizer.selectNext()
                 if self.tokenizer.next.type == 'O_PAR':
@@ -242,7 +361,7 @@ class Parser:
                     result = BinOp('*', [result, IntVal(int(self.tokenizer.next.value))])
                 else:
                     raise Exception('Algo de estranho aconteceu, confira a entrada')
-            else:
+            elif self.tokenizer.next.type == 'DIV':
                 self.tokenizer.selectNext()
                 if self.tokenizer.next.type == 'O_PAR':
                     result = BinOp('/', [result, self.parseFactor()])
@@ -251,22 +370,41 @@ class Parser:
                     result = BinOp('/', [result, IntVal(int(self.tokenizer.next.value))])
                 else:
                     raise Exception('Algo de estranho aconteceu, confira a entrada')
-        
+            elif self.tokenizer.next.type == 'AND':
+                self.tokenizer.selectNext()
+                result = BinOp('&&', [result, self.parseFactor()])
+                return result
             self.tokenizer.selectNext()
         return result
     
     def parseExpression(self):
         result = self.parseTerm()
-        while self.tokenizer.next.type == 'ADD' or self.tokenizer.next.type == 'SUB':
+        while self.tokenizer.next.type == 'ADD' or self.tokenizer.next.type == 'SUB' or self.tokenizer.next.type == 'OR':
             if self.tokenizer.next.type == 'ADD':
                 self.tokenizer.selectNext()
                 result = BinOp('+', [result, self.parseTerm()])
-            else:
+            elif self.tokenizer.next.type == 'SUB':
                 self.tokenizer.selectNext()
                 result = BinOp('-', [result, self.parseTerm()])
+            elif self.tokenizer.next.type == 'OR':
+                self.tokenizer.selectNext()
+                result = BinOp('||', [result, self.parseTerm()])
         return result
     
-    
+    def parserRelExpr(self):
+        result = self.parseExpression()
+        while self.tokenizer.next.type == 'EQUAL' or self.tokenizer.next.type == 'GREATER' or self.tokenizer.next.type == 'LESS':
+            if self.tokenizer.next.type == 'EQUAL':
+                self.tokenizer.selectNext()
+                result = BinOp('==', [result, self.parseExpression()])
+            elif self.tokenizer.next.type == 'GREATER':
+                self.tokenizer.selectNext()
+                result = BinOp('>', [result, self.parseExpression()])
+            elif self.tokenizer.next.type == 'LESS':
+                self.tokenizer.selectNext()
+                result = BinOp('<', [result, self.parseExpression()])
+        return result
+
     @staticmethod
     def run(self):
         result = self.parseBlock()
